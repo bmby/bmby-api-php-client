32dd6287054753bfab1b34a2c8e8cdcb6216a308
Martin Boyakhchyan <martin@bmby.com>
2019-01-01 12:31:23 +0200

    time line sync with bmby

---
+++ src/Bmbyhood/Rest/QueryParams/QueryParams.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 31/12/2018
+ * Time: 11:40
+ */
+
+namespace Bmbyhood\Rest\QueryParams;
+
+
+class QueryParams
+{
+
+}
---
+++ src/Bmbyhood/Rest/QueryParams/TimeLineSummaryQueryParams.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 31/12/2018
+ * Time: 11:51
+ */
+
+namespace Bmbyhood\Rest\QueryParams;
+
+
+class TimeLineSummaryQueryParams
+{
+
+}
